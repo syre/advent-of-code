656805329d41091d65211cdb524cf6ca8953c572
Søren Howe Gersager <thesyre@pm.me>
2025-12-04 20:28:12 +0100

    new iteration of day 1 - part 2

--- 2025_go/day_01/day_01.go
+++ 2025_go/day_01/day_01.go
@@ -19,10 +19,8 @@ func part_one() {
 	string_lists := strings.Split(string(input), "\n")
 	dial_val := 50
 	zero_counter := 0
-	part_two_zero_counter := 0
 
 	println("dial starts at", dial_val)
-
 	for _, line := range string_lists {
 		if line == "" {
 			continue
@@ -33,17 +31,12 @@ func part_one() {
 
 		if direction == "L" {
 			if dial_val-distance < 0 {
-				part_two_zero_counter += (int_abs(dial_val-distance) / 100)
-
-				fmt.Println("added to part_two zero counter", (int_abs(dial_val-distance) / 100))
 				dial_val = (100 - int_abs(dial_val-distance)) % 100
 			} else {
 				dial_val -= distance
 			}
 		} else if direction == "R" {
 			if dial_val+distance > 99 {
-				part_two_zero_counter += ((dial_val + distance) / 100)
-				fmt.Println("added to part_two zero counter", (dial_val+distance)/100)
 				dial_val = (dial_val + distance) % 100
 			} else {
 				dial_val += distance
@@ -57,9 +50,50 @@ func part_one() {
 		println("dial is rotated", direction, distance, "to point at", dial_val)
 	}
 	println("zero counter at", zero_counter)
-	println("part two zero counter at", part_two_zero_counter)
+}
+
+func part_two() {
+	input, _ := os.ReadFile("input")
+	string_lists := strings.Split(string(input), "\n")
+	dial_val := 50
+	zero_counter := 0
+
+	println("dial starts at", dial_val)
+	for _, line := range string_lists {
+		if line == "" {
+			continue
+		}
+
+		direction := string(line[0])
+		distance, _ := strconv.Atoi(line[1:])
+
+		if direction == "L" {
+			for i := distance; i >= 0; i-- {
+				if dial_val == -1 {
+					fmt.Println("incrementing zero counter")
+					zero_counter += 1
+					dial_val = 99
+					continue
+				}
+				dial_val -= 1
+			}
+		} else if direction == "R" {
+			for i := 0; i < distance; i++ {
+				if dial_val == 99 {
+					fmt.Println("incrementing zero counter")
+					zero_counter += 1
+					dial_val = 0
+					continue
+				}
+				dial_val += 1
+			}
+		}
+		println("dial is rotated", direction, distance, "to point at", dial_val)
+	}
+	println("zero counter at", zero_counter)
 }
 
 func main() {
-	part_one()
+	//part_one()
+	part_two()
 }
