b0e4bdbeab06ed0dfadc202c469ecb6a6ae20d54
Søren Howe Gersager <thesyre@pm.me>
2025-12-06 09:06:52 +0100

    one more wip

--- 2025_go/day_06/day_06.go
+++ 2025_go/day_06/day_06.go
@@ -7,6 +7,12 @@ import (
 	"strings"
 )
 
+func print_2d_array(arr [][]string) {
+	for _, row := range arr {
+		fmt.Println(row)
+	}
+}
+
 func main() {
 	input, _ := os.ReadFile("test_input")
 	problem_input := strings.Split(string(input), "\n")
@@ -14,17 +20,28 @@ func main() {
 	// populate problem matrix
 	for index, val := range problem_input {
 		problem_matrix = append(problem_matrix, []string{})
-		splitted := strings.Split(val, " ")
+		splitted := strings.Fields(val)
 		for _, el := range splitted {
-			trimmed_el := strings.TrimSpace(el)
-			problem_matrix[index] = append(problem_matrix[index], trimmed_el)
+			problem_matrix[index] = append(problem_matrix[index], el)
 		}
 	}
-	fmt.Println(problem_matrix)
+	print_2d_array(problem_matrix)
+
+	total_sum := 0
 	for i := 0; i < len(problem_matrix)-1; i++ {
-		for j := 0; j < len(problem_matrix[0])-1; j++ {
-			number, _ := strconv.Atoi(strings.TrimSpace(problem_matrix[j][i]))
-			fmt.Println(number)
+		column_sum := 0
+		for j := 0; j < len(problem_matrix[i])-1; j++ {
+			number, _ := strconv.Atoi(problem_matrix[j][i])
+
+			operator := problem_matrix[len(problem_matrix[0])-1][i]
+			if operator == "*" {
+				column_sum *= number
+			} else if operator == "+" {
+				column_sum += number
+			}
 		}
+		fmt.Println(column_sum)
+		total_sum += column_sum
 	}
+	fmt.Println(total_sum)
 }
